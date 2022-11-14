0b0b871898deb826861c84e362b330cb91928e99
tianiita <110095686+tianiita@users.noreply.github.com>
2022-11-14 10:02:26 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -26,7 +26,7 @@ submitBtn.addEventListener('click', ev => {
     fetchRandom();
 
     const toastElList = [].slice.call(document.querySelectorAll('.toast'));
-    const toastList = toastElList.map(function (toastEl) {
+    const toastList = toastElList.map((toastEl) => {
         return new bootstrap.Toast(toastEl);
     });
     toastList.forEach(toast => toast.show());
